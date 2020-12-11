e5339182df0159ad43ab93115e9ab5a8d645bf95
Joseph Abraham <ekurubal24@gmail.com>
2020-12-11 15:49:49 +0300

    Add user add function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,7 +11,22 @@ exports.get_users = async function(req, res, next) {
   }
 };
 
-
-exports.add_user = (reg, res) => {
+// on get request
+exports.show_add_user_form = (req, res) => {
   res.render("addUser"); 
+};
+
+// on post request
+
+exports.add_user = async (req, res) => {
+  // add to db
+  try {
+    const newUser = await UserModel.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+    res.redirect("/users");
+  } catch (error) {
+    res.send("An error occured.");
+  }
 };
--- models/User.js
+++ models/User.js
@@ -2,34 +2,37 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 //connect to db
 // postgres://user:pass@example.com:5432/dbname
-
 const { DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DB_PORT, DB_NAME } = process.env;
-
 const sequelize = new Sequelize(
   `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}`
 );
-
 sequelize
   .authenticate()
   .then(() => console.log("Successfully connected to database"))
   .catch((err) => console.log("Unable to connect to database", err));
 
+// ORM table
+// recruiter table in the clarusway db
 
+// modelname, attribute, options
 const UserModel = sequelize.define(
-    "recruiter",
-    {
-    //attribute
-    //id, fristName, lastName, createAt, updatedAt
-    firstName:{
-        type: DataTypes.STRING,
+  "recruiters",
+  {
+    //attributes
+    //id, firstName, lastName, createdAt, updatedAt
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    lastName:{
-        type:DataTypes.STRING,
-    }
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    {
+  },
+  {
     //options
-    }
+    //freezeTableName: true,
+  }
 );
 
 module.exports = UserModel;
