2227063b939f4c65f43cf5ced7125a01bdbac225
Joseph Abraham <ekurubal24@gmail.com>
2020-12-11 17:42:37 +0300

    Add user delete

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const usersController = require("../controllers/usersController");
 
-/* GET users listing. */
-router.get('/', usersController.get_users);
+/*  /users */
 
+router.get("/", usersController.get_users);
 
-router.get('/add', usersController.show_add_user_from);
-router.post('/add', usersController.add_user);
+router.get("/add", usersController.show_add_user_form);
+router.post("/add", usersController.add_user);
 
+router.get("/:id/delete", usersController.delete_user);
 
 
 module.exports = router;
